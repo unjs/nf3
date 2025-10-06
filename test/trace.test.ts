2f08f35b945d393106aa527df3bfa94aa97cb996
Andrew Lisowski <lisowski54@gmail.com>
2025-10-06 12:52:28 -0700

    Add fullTraceInclude option

--- test/trace.test.ts
+++ test/trace.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from "vitest";
 import { traceNodeModules } from "../src/index.ts";
 import { fileURLToPath } from "node:url";
 import { cp } from "node:fs/promises";
+import path from "node:path";
+import { readFile } from "node:fs/promises";
 
 describe("traceNodeModules", () => {
   it("traceNodeModules", async () => {
@@ -20,5 +22,26 @@ describe("traceNodeModules", () => {
       extraUtils: "@fixture/nitro-utils/extra",
       subpathLib: "@fixture/nitro-lib@2.0.0",
     });
+  });
+
+  it.only("traceNodeModules with fullTraceInclude", async () => {
+    const input = fileURLToPath(new URL("fixture/index.mjs", import.meta.url));
+    const outDir = fileURLToPath(new URL("dist/trace", import.meta.url));
+
+    await cp(input, `${outDir}/index.mjs`);
+    await traceNodeModules([input], { outDir, fullTraceInclude: true });
+
+    expect(
+      await readFile(
+        path.join(
+          outDir,
+          "node_modules",
+          "@fixture",
+          "nitro-utils",
+          "README.md",
+        ),
+        { encoding: "utf8" },
+      ),
+    ).toMatch("# TEST");
   });
 });
